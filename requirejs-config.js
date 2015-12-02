bd9ed828039594c30eb38717125504fdca3c7c0e
Quinten Clause <quinten.clause@studioemma.eu>
2015-12-02 12:47:28 +0100

    Added slideshow that previously reside in the module

---
+++ requirejs-config.js
@@ -0,0 +1,14 @@
+/*
+ *
+ * Include javascript files
+ *
+ */
+
+var config = {
+    paths: {
+        responsiveslides: 'StudioEmma_Optimus/js/responsiveslides.min'
+    },
+    shim: {
+        responsiveslides: ['jquery']
+    }
+};
