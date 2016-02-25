e47d152e65198122fb28614836a6afaf648c834b
Quinten Clause <quinten.clause@studioemma.eu>
2016-02-25 15:59:55 +0100

    Support voor meerdere popup-gallery op 1 pagina

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -60,22 +60,26 @@ define([
         });
 
         require(['magnificpopup'], function(){
-            $('.popup-gallery').magnificPopup({
-                delegate: 'a',
-                type: 'image',
-                tLoading: 'Loading image #%curr%...',
-                mainClass: 'mfp-img-mobile',
-                gallery: {
-                    enabled: true,
-                    navigateByImgClick: true,
-                    preload: [0,1]
-                },
-                image: {
-                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                    titleSrc: function(item) {
-                        return (item.el.attr('title') === undefined) ? '' : item.el.attr('title');
+
+            $('.popup-gallery').each(function (index){
+
+                $(this).magnificPopup({
+                    delegate: 'a',
+                    type: 'image',
+                    tLoading: 'Loading image #%curr%...',
+                    mainClass: 'mfp-img-mobile',
+                    gallery: {
+                        enabled: true,
+                        navigateByImgClick: true,
+                        preload: [0,1]
+                    },
+                    image: {
+                        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                        titleSrc: function(item) {
+                            return (item.el.attr('title') === undefined) ? '' : item.el.attr('title');
+                        }
                     }
-                }
+                });
             });
         });
     }
