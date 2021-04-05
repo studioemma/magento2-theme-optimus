645e70bbe6ac4bca5e59af4994a273c81683b9d9
Klaus De Buysser <klaus@studioemma.com>
2021-04-05 22:11:36 +0200

    Add fix for UI tabs error in console

--- requirejs-config.js
+++ requirejs-config.js
@@ -8,6 +8,17 @@ var config = {
         'js/responsive-optimus',
         'js/limitContent'
     ],
+    map: {
+        "*": {
+            // if someone depends on jquery/ui, jquery/compat is loaded
+            // jquery/compat loads jquery-ui-modules/tabs
+            // if jquery-ui-modules/tabs is loaded after mage/tabs, the tabs function is mapped to the wrong widget
+            // (to the jquery widget and not to the Magento widget)
+            // then, it is not possible to call methods on the tabs
+            // this mapping fixes this issue, because jquery-ui-modules/tabs is not loaded any more
+            "jquery-ui-modules/tabs": "mage/tabs"
+        }
+    },
     paths: {
         responsiveslides: 'js/responsiveslides.min',
         magnificpopup: 'js/jquery.magnific-popup.min',
